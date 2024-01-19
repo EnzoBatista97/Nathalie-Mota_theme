1232708a0c88040d1f46c47fc2475a5b1f94b50b
Enzo Batista <batista.enzo.be@gmail.com>
2024-01-19 10:42:17 +0100

    Modal de contact terminée

--- templates_part/modal-contact.php
+++ templates_part/modal-contact.php
@@ -3,7 +3,7 @@
 <div id="contact-modal" class="modal">
     <div class="modal-content">
         <span class="close-modal">&times;</span>
-        <h2>Contact</h2>
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Contact_header.svg" alt="Contact header">
         <?php echo do_shortcode('[contact-form-7 id="36d3c80" title="Untitled"]'); ?>
     </div>
 </div>
